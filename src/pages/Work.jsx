5a99075424899354952756fa8709312d2c0319e7
rajibulislam60 <rajibulislam60707877@gmail.com>
2026-05-04 01:27:12 +0600

    work area design done

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../container/Container";
 import ProjectArea from "../Components/ProjectArea";
+import ProjectImage from "../images/pr03-02.webp";
+import { FaArrowRight } from "react-icons/fa";
 
 const Work = () => {
+  const [scrollX, setScrollX] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollX(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // limit movement (important)
+  const moveX = Math.min(scrollX * 0.2, 300);
+
   return (
     <div className="py-16">
       <Container>
@@ -18,13 +34,36 @@ const Work = () => {
             </h3>
           </div>
 
-          <h2 className="text-3xl sm:text-5xl lg:text-7xl font-bold">
+          <h2 className="text-5xl lg:text-7xl font-bold">
             Featured case studies
           </h2>
         </div>
 
         <div>
           <ProjectArea />
+        </div>
+
+        <div className="flex justify-between items-center mt-6 lg:mt-16 overflow-hidden">
+          <FaArrowRight
+            className="text-[0px] lg:text-[300px] text-gray-300 lg:px-6 transition-transform duration-200"
+            style={{
+              transform: `translateX(${moveX}px)`,
+            }}
+          />
+
+          <div>
+            <h3 className="tracking-widest uppercase text-sm lg:text-lg font-semibold">
+              [ All Works ]
+            </h3>
+
+            <div className="w-[250px] sm:w-[350px] h-[150px] sm:h-[200px] mt-3">
+              <img
+                className="w-full h-full object-cover"
+                src={ProjectImage}
+                alt="Project Image"
+              />
+            </div>
+          </div>
         </div>
       </Container>
     </div>
